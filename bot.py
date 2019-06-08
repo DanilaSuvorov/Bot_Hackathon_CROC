4ced8e2b42ef28ad8129ec9fcd6775f6a165e055
DanilaSuvorov <dansuvtim@gmail.com>
2019-06-08 14:10:43 +0300

    kk

--- bot.py
+++ bot.py
@@ -1,61 +1,59 @@
-import logging
-from telegram.ext import Updater,  MessageHandler, Filters
-from urllib3 import make_headers
-alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
-request_kwargs = {
-    'proxy_url': 'http://80.152.210.221:3128',
-    'urllib3_proxy_kwargs': {
-        'proxy_headers': make_headers(proxy_basic_auth='username:password')
-    }
-}
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-updater = Updater(token='714985193:AAGRTr1nYeOB2Hf1v0SkVs8xR3yrwyQ_tEg', use_context=True, request_kwargs=request_kwargs)
+import vk_api
+import requests
+import matplotlib.pyplot as plt
+
+from vk_api.longpoll import VkLongPoll, VkEventType
+session = requests.Session()
+vk_session = vk_api.VkApi(token='370afd337d846f58deb813810ae22a2b9f756c28155593c48496e4d691d176176a8d03f28b0ae5161f047')
+longpoll = VkLongPoll(vk_session)
+vk = vk_session.get_api()
+for event in longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+        if event.text == '/start':
+            if event.from_user:
+                vk.messages.send(
+                    random_id=187823456,
+                    user_id=event.user_id,
+                    message='Сколько раз в день мне спрашивать ваше настроение?')
+        if event.text != '/start':
+            if event.from_user:
+                count = event.text
+                print(count)
+                vk.messages.send(
+                    random_id=187823456,
+                    user_id=event.user_id,
+                    message='Когда вы просыпаетесь?')
+        if event.text.:
+            if event.from_user:
+                count = event.text
+                print(count)
+                vk.messages.send(
+                    random_id=187823456,
+                    user_id=event.user_id,
+                    message='Когда вы просыпаетесь?')
+time = []
+
+def time():
+    if count == 1:
+        time.append((y - x) / 2 + x)
+    elif count == 2:
+        time.append(x + 2)
+        time.append(y - 1)
+    elif count == 3:
+        time.append((y - x) / 2 + x)
+        time.append(x + 2)
+        time.append(y - 1)
+    elif count >= 4:
+        many = (y - x) / count
+        for i in range(x, y):
+            time.append(x + many)
 
 
-def handle_message(update, context):
-    chat_id = update.message.chat_id
-    text = update.message.text
-    if text[0:8] == "Зашифруй":
-        print(text.split("("))
-        count = text.split("(")[1]
-        count = count[0:-1]
-        text_to_cipher = text.split("(")[2]
-        text_to_cipher = text_to_cipher[0:-1]
-        result = cipher(text_to_cipher, count)
-        context.bot.send_message(chat_id, result)
-    elif text[0:9] == "Расшифруй":
-        print(text.split("("))
-        count = text.split("(")[1]
-        count = count[0:-1]
-        text_to_un_cipher = text.split("(")[2]
-        text_to_un_cipher = text_to_un_cipher[0:-1]
-        result = un_cipher(text_to_un_cipher, count)
-        context.bot.send_message(chat_id, result)
 
 
-handler = MessageHandler(Filters.text | Filters.command, handle_message)
-updater.dispatcher.add_handler(handler)
-updater.start_polling()
-
-
-def cipher(text, counter):
-    text.lower()
-
-    res = []
-    for i in text:
-        if i == " ":
-            res.append(" ")
-        else:
-            res.append(alphabet[(alphabet.index(i) + int(counter)) % 33])
-    return "".join(res)
-
-
-def un_cipher(text, counter):
-    text.lower()
-    res = []
-    for i in text:
-        if i == " ":
-            res.append(" ")
-        else:
-            res.append(alphabet[(alphabet.find(i) - int(counter)) % 33])
-    return "".join(res)
+def graphic():
+    fig, ax = plt.subplots(1, 1)
+    ax.plot(time[], vkapi['likes'])
+    fig.set_size_inches(22, 10)
+    plt.title('Статистика по максимальному значению лайков за год', fontsize=18)
+    plt.savefig('likes.png')
